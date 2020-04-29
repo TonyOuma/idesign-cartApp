68eb3ea38c832668463c480fc8aa9d9acd9dca73
TonyOuma <kyrietony@gmail.com>
2020-04-30 01:23:27 +0300

    Styled the cart and added add to cart functionalities

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const cartDOM = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
-const cartContent = document.querySelector(".cart-Content");
+const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
 
 // cart
@@ -87,7 +87,9 @@ class UI {
         // set cart values
         this.setCartValues(cart);
         // display cart item
+        this.addCartItem(cartItem);
         // show the cart
+        this.showCart();
       });
     });
   }
@@ -102,8 +104,30 @@ class UI {
 
     cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
     cartItems.innerText = itemsTotal;
-
-    console.log(cartTotal, cartItems);
+  }
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `
+         <img src=${item.image} alt="product">
+                    <div>
+                        <h4>${item.title}</h4>
+                        <h5>$${item.price}</h5>
+                        <span class="remove-item" data-id=${item.id}>
+                            <i class="fas fa-trash"></i>
+                        </span>
+                    </div>
+                    <div>
+                        <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                        <p class="item-amount">${item.amount}</p>
+                        <i class="fas fa-chevron-down" data-id=${item.id}></i>
+                    </div>
+        `;
+    cartContent.appendChild(div);
+  }
+  showCart() {
+    cartOverlay.classList.add("transparentBcg");
+    cartDOM.classList.add("showCart");
   }
 }
 // local storage
